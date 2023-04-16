d6bdfdfe0cd3c4114fbc010426b36e0a227fb39b
roggen <robin@seerig.de>
2023-04-17 01:45:04 +0200

    deleted unnecessary

--- libs/app/src/node_style.rs
+++
@@ -1,9 +0,0 @@
-// nodium_ui/src/node_style.rs
-
-pub struct NodeStyle {
-  pub category: String,
-  pub color: String,
-  pub icon: String,
-  pub label: String,
-  // Add any other style properties you'd like to support
-}
